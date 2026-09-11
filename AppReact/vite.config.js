f4632770cf4ab7007ca907910571221917194256
Nnlllllll <nathannovais020@gmail.com>
2026-09-11 18:09:17 +0000

    gh

--- AppReact/vite.config.js
+++ AppReact/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    allowedHosts: ['bentogo.me'], // Libera o acesso ao domínio
-    host: true, // Garante que ele ouça na rede do container
-    port: 5173
-  },
-  assetsInclude: ['**/*.glb'],
+  assetsInclude: ['**/Bentao.glb'],
 })
